ffadb7b5c62790db3df9c2e7788666be8427319a
hozlucas28 <hozlucas28@gmail.com>
2022-09-22 19:59:06 -0300

    Upload lesson

---
+++ Clases/15/JS/functions.js
@@ -0,0 +1,29 @@
+
+/* -------------------------------- Funciones ------------------------------- */
+
+function add() {
+    let result = document.getElementById('result'),
+        resultValue = Number(result.innerHTML);
+    
+    resultValue++;
+    result.innerText = resultValue;
+    changeParityValue(resultValue);
+};
+
+
+function subtract() {
+    let result = document.getElementById('result'),
+        resultValue = Number(result.innerHTML);
+    
+    resultValue--;
+    result.innerText = resultValue;
+    changeParityValue(resultValue);
+};
+
+
+function changeParityValue(number = 0) {
+    let parity = document.getElementById('parity');
+    let operation = number % 2;
+
+    (operation === 0)? parity.innerHTML = 'es par.' : parity.innerHTML = 'es impar.';
+};
