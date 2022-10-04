514d8420a084f07d3e10da70ebe02786b54a2db4
hozlucas28 <hozlucas28@gmail.com>
2022-10-04 20:26:05 -0300

    Upload lesson

---
+++ Clases/18/JS/app.js
@@ -0,0 +1,34 @@
+
+/* ----------------------------- Elementos HTML ----------------------------- */
+
+const buttonAdd = document.getElementById('attach-texts_button-add');
+
+const getInput = () => document.getElementById('attach-texts__input-text');
+const getListOfAttached = () => document.getElementById('attach-texts__attached-content__list');
+const getInputValue = () => getInput().value;
+
+
+
+/* --------------------------------- Eventos -------------------------------- */
+
+buttonAdd.addEventListener('click', () => {
+    let input = getInput(),
+        inputValue = getInputValue(),
+        listOfAttached = getListOfAttached();
+
+    if (inputValue === '')
+        return;
+
+        addToList(listOfAttached, inputValue);
+        input.value = '';
+});
+
+
+
+/* -------------------------------- Funciones ------------------------------- */
+
+function addToList(listOfAttached, inputValue) {
+    let li = document.createElement("li");
+    listOfAttached.append(li);
+    li.innerHTML = inputValue;
+}
